a98ae9fd9f494ab66ac176028b92ce416d534f8d
prajoshjohny10329 <prajoshjohny10329@gmail.com>
2023-07-24 18:14:11 +0530

    mongodb updated4

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ const state={db:null}
 
 module.exports.connect=(done)=>{
     // const url='mongodb://127.0.0.1:27017'
-  const url='mongodb+srv://prajoshjohny10329:9foRxqmu28qJx9bb@cluster0.drxsbfg.mongodb.net/'
+  const url='mongodb://prajoshjohny10329:9foRxqmu28qJx9bb@cluster0.drxsbfg.mongodb.net/'
     const dbName='Appleshop'
 
     MongoClient.connect(url,(err,data)=>{
